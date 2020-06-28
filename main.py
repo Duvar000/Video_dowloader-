3c84d52c9421df138b72e32e84b6878dcb776f15
İsmail Arındı <66697982+Duvar000@users.noreply.github.com>
2020-06-28 19:43:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,44 +6,43 @@ from pytube import YouTube
 windows = QApplication([])
 screen = uic.loadUi("windows.ui")
 
-def Dowload_360p():
-    try:
-        link = screen.lineEdit.text()
-        youtube = YouTube(link)
-        print(youtube.title , "dowloading...")
-        stream = youtube.streams.get_by_itag(18) 
-        stream.download("video")
-        print("sccussfully!")
-    except:
-        print("error")
-def Dowload_720p():
-    try:
-        link = screen.lineEdit.text()
-        youtube = YouTube(link)
-        print(youtube.title , "dowloading...")
-        stream = youtube.streams.get_by_itag(22) 
-        stream.download("video")
-        print("sccussfully!")
-    except:
-        print("error")
-def dowload_mp3 ():
-    try:
-        link = screen.lineEdit.text()
-        youtube = YouTube(link)
-        print(youtube.title , "dowloading...")
-        stream = youtube.streams.get_by_itag(251) 
-        video = stream.download("video")
-        video
-        os.rename(video , youtube.title+".mp3")
-        print("sccussfully!")
-    except:
-        print("error")
+def Dowload():
+    if screen.comboBox.currentText() == "360p":
+        try:
+            link = screen.lineEdit.text()
+            youtube = YouTube(link)
+            print(youtube.title , "dowloading...")
+            stream = youtube.streams.get_by_itag(18) 
+            stream.download("video")
+            print("sccussfully!")
+        except:
+            print("error")
+    if screen.comboBox.currentText() == "720p":
+        try:
+            link = screen.lineEdit.text()
+            youtube = YouTube(link)
+            print(youtube.title , "dowloading...")
+            stream = youtube.streams.get_by_itag(22) 
+            stream.download("video")
+            print("sccussfully!")
+        except:
+            print("error")
+    if screen.comboBox.currentText() == "mp3":
+        try:
+            link = screen.lineEdit.text()
+            youtube = YouTube(link)
+            print(youtube.title , "dowloading...")
+            stream = youtube.streams.get_by_itag(251) 
+            video = stream.download("video")
+            video
+            os.rename(video , youtube.title+".mp3")
+            print("sccussfully!")
+        except:
+            print("error")
 
 
 
-screen.pushButton_1.clicked.connect(dowload_mp3)
-screen.pushButton_2.clicked.connect(Dowload_360p)
-screen.pushButton_3.clicked.connect(Dowload_720p)
+screen.pushButton.clicked.connect(Dowload)
 
 screen.show()
 windows.exec_()
